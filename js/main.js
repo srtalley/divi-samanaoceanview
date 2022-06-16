fd7513afe6867aa16238a424bbfde25aeb594619
Steve Talley <steve@dustysun.com>
2022-06-16 00:53:03 -0500

    v1.0.5

--- js/main.js
+++ js/main.js
@@ -14,6 +14,29 @@ jQuery(function($) {
     $(document).on('click', '.ui-datepicker-week-end.appointable ~ .ui-datepicker-unselectable.not_appointable.appointable', function (e) {
       $(this).closest('tr').find('.ui-datepicker-week-end.appointable').click();
     });
+
+    // Create an observer instance to add classes to the selected and next weeks
+    var observer = new MutationObserver(function( mutations ) {
+      mutations.forEach(function( mutation ) {	
+          mutation.addedNodes.forEach(
+            function(node) {
+                if( $(node).hasClass("ui-datepicker-calendar")){	
+                    $(node).find('.highlighted_day').parent().addClass('selected-week');
+                    $(node).find('.highlighted_day').parent().next('tr').addClass('next-week');
+                }
+            }
+        );	
+      });  
+    });
+    // Configuration of the observer:
+    var config = { 
+        childList: true,
+        attributes: true,
+        subtree: true,
+        characterData: true
+    }; 
+    var targetNode = $('.ui-datepicker-inline')[0];
+    observer.observe(targetNode, config);  
   }
    /**
      * Function for adding days to a date
@@ -29,52 +52,52 @@ jQuery(function($) {
 
     var form = $('.wc-appointments-appointment-form-wrap.cart');
     // get changed durations
-    form.on( 'addon-duration-changed', function(event, duration) {
-        var currentForm = $(this);
+    // form.on( 'addon-duration-changed', function(event, duration) {
+    //     var currentForm = $(this);
 
-        var addon_duration = parseInt( duration, 10 );
-        var checkin_checkout_div = currentForm.find('.wc-appt-checkin-checkout-dates');
+    //     var addon_duration = parseInt( duration, 10 );
+    //     var checkin_checkout_div = currentForm.find('.wc-appt-checkin-checkout-dates');
 
-        var type = 'other';
-        if(checkin_checkout_div.hasClass('type-reservation')) {
-            addon_duration++;
-            type = 'reservation';
-        }
-        var endDate = addDays(selectedDate, addon_duration);
-        var month = endDate.toLocaleString('default', { month: 'long' });
+    //     var type = 'other';
+    //     if(checkin_checkout_div.hasClass('type-reservation')) {
+    //         addon_duration++;
+    //         type = 'reservation';
+    //     }
+    //     var endDate = addDays(selectedDate, addon_duration);
+    //     var month = endDate.toLocaleString('default', { month: 'long' });
 
-        if(type == 'reservation') {
-            $(currentForm).find('.checkout-value').html('11-12 PM ' + month + ' ' + endDate.getDate().toString() + ', ' + endDate.getFullYear().toString());
-        } else {
-            $(currentForm).find('.checkout-value').html(month + ' ' + endDate.getDate().toString() + ', ' + endDate.getFullYear().toString());
-        }
+    //     if(type == 'reservation') {
+    //         $(currentForm).find('.checkout-value').html('11-12 PM ' + month + ' ' + endDate.getDate().toString() + ', ' + endDate.getFullYear().toString());
+    //     } else {
+    //         $(currentForm).find('.checkout-value').html(month + ' ' + endDate.getDate().toString() + ', ' + endDate.getFullYear().toString());
+    //     }
 
-        // see if there are unavailble dates that have been selected
-        var selected_days = $(currentForm).find('.ui-datepicker-selected-day');
-        $(selected_days).each(function() {
-            if($(this).hasClass('fully_scheduled') || $(this).hasClass('not_appointable')) {
-                $(currentForm).find('.wc-appt-error').slideDown();
-                $(currentForm).find('.wc-appointments-appointment-form-button').prop('disabled', true);
-            } else {
-                $(currentForm).find('.wc-appt-error').slideUp();
-                $(currentForm).find('.wc-appointments-appointment-form-button').prop('disabled', false);
-            }
-        });
-        if(type == 'reservation') {
-            $(currentForm).find('.ui-datepicker-checkout-day').removeClass('ui-datepicker-checkout-day');
+    //     // see if there are unavailble dates that have been selected
+    //     var selected_days = $(currentForm).find('.ui-datepicker-selected-day');
+    //     $(selected_days).each(function() {
+    //         if($(this).hasClass('fully_scheduled') || $(this).hasClass('not_appointable')) {
+    //             $(currentForm).find('.wc-appt-error').slideDown();
+    //             $(currentForm).find('.wc-appointments-appointment-form-button').prop('disabled', true);
+    //         } else {
+    //             $(currentForm).find('.wc-appt-error').slideUp();
+    //             $(currentForm).find('.wc-appointments-appointment-form-button').prop('disabled', false);
+    //         }
+    //     });
+    //     if(type == 'reservation') {
+    //         $(currentForm).find('.ui-datepicker-checkout-day').removeClass('ui-datepicker-checkout-day');
 
-            var last_selected_day = $(selected_days).last();
+    //         var last_selected_day = $(selected_days).last();
 
-            if(last_selected_day.length) {
-                $(currentForm).find('.ui-datepicker-checkout-day').removeClass('ui-datepicker-checkout-day');
-                var add_checkout_day = last_selected_day.nextAll( 'td' ).add( last_selected_day.closest( 'tr' ).nextAll().find( 'td' ) ).slice( 0, 1 ).addClass( 'ui-datepicker-checkout-day' );
-            } else {
-                var selected_day = $(currentForm).find('.ui-datepicker-current-day');
-                var add_checkout_day = selected_day.nextAll( 'td' ).add( selected_day.closest( 'tr' ).nextAll().find( 'td' ) ).slice( 0, 1 ).addClass( 'ui-datepicker-checkout-day' );
-            }
+    //         if(last_selected_day.length) {
+    //             $(currentForm).find('.ui-datepicker-checkout-day').removeClass('ui-datepicker-checkout-day');
+    //             var add_checkout_day = last_selected_day.nextAll( 'td' ).add( last_selected_day.closest( 'tr' ).nextAll().find( 'td' ) ).slice( 0, 1 ).addClass( 'ui-datepicker-checkout-day' );
+    //         } else {
+    //             var selected_day = $(currentForm).find('.ui-datepicker-current-day');
+    //             var add_checkout_day = selected_day.nextAll( 'td' ).add( selected_day.closest( 'tr' ).nextAll().find( 'td' ) ).slice( 0, 1 ).addClass( 'ui-datepicker-checkout-day' );
+    //         }
 
-        }
-    } );
+    //     }
+    // } );
 
     // get the start date
     form.on( 'date-selected', function(event, data) {
@@ -88,17 +111,16 @@ jQuery(function($) {
             var checkin_checkout_div = currentForm.find('.wc-appt-checkin-checkout-dates');
 
             // reset the added days when changing a date
-
-                var type = 'reservation';
-                var appointment_duration = picker.data( 'appointment_duration' ) ? parseInt(picker.data( 'appointment_duration' )) : 7;
+            var type = 'reservation';
+            // add one day to the reservation length
+            var appointment_duration = picker.data( 'appointment_duration' ) ? parseInt(picker.data( 'appointment_duration' )) + 1 : 8;
             
-
             var month = selectedDate.toLocaleString('default', { month: 'long' });
 
-              var endDate = addDays(selectedDate, appointment_duration);
-              var endMonth = endDate.toLocaleString('default', { month: 'long' });
-              $(currentForm).find('.checkin-value').html('3 - 4PM, ' + month + ' ' + date[2] + ', ' + date[0]);
-              $(currentForm).find('.checkout-value').html('11 - 12 PM, ' + endMonth + ' ' + endDate.getDate().toString() + ', ' + endDate.getFullYear().toString());
+            var endDate = addDays(selectedDate, appointment_duration);
+            var endMonth = endDate.toLocaleString('default', { month: 'long' });
+            $(currentForm).find('.checkin-value').html('3 - 4PM, ' + month + ' ' + date[2] + ', ' + date[0]);
+            $(currentForm).find('.checkout-value').html('11 - 12 PM, ' + endMonth + ' ' + endDate.getDate().toString() + ', ' + endDate.getFullYear().toString());
            
         } // end day
    
